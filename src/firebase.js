9c538ebc2f1e505d083f1e24d83c694143d8b501
adamazmi2207 <66166999+adamazmi2207@users.noreply.github.com>
2020-12-13 13:43:47 +0900

    Initial commit
    WhatsApp Clone with React JS!

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBlkFu-rkWLY-yrhsx0C1VQ2Vf_Nb2A0lU",
+    authDomain: "whatsapp-clone-ad646.firebaseapp.com",
+    projectId: "whatsapp-clone-ad646",
+    storageBucket: "whatsapp-clone-ad646.appspot.com",
+    messagingSenderId: "1010149062905",
+    appId: "1:1010149062905:web:01dcd226116c1388178a28"
+  };
